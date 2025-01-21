bf6fb23098bae71542fb05c023d96b94a6d25b40
gonzagaa <iigust4vo@gmail.com>
2025-01-21 11:41:52 -0300

    update

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-
 const larguraDaTela = window.innerWidth
 
 if (larguraDaTela < 800) {
   var swiper4 = new Swiper(".mySwiper4", {
     cssMode: true,
+    spaceBetween: 10,
       navigation: {
         nextEl: ".swiper-button-next",
         prevEl: ".swiper-button-prev",
@@ -16,19 +16,46 @@ if (larguraDaTela < 800) {
       loop: true,
   });
 } else {
-  var swiper4 = new Swiper(".mySwiper4", {
-      slidesPerView: 3,
-      spaceBetween: 20,
-      loop: true,
-      grabCursor: true,
-      freeMode: false,
-      pagination: {
-        el: ".swiper-pagination",
-        clickable: true,
-      },
-      navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
-      },
-    });
+  // Remove as classes do carrossel para desktop
+  document.querySelectorAll('.swiper, .mySwiper4').forEach(el => {
+    el.classList.remove('swiper', 'mySwiper4');
+  });
+
+  document.querySelectorAll('.swiper-wrapper').forEach(el => {
+    el.classList.remove('swiper-wrapper');
+  });
+
+  document.querySelectorAll('.swiper-slide').forEach(el => {
+    el.classList.remove('swiper-slide');
+  });
 }
+
+// Seleciona os elementos
+const openModalButtons = document.querySelectorAll('.openModalForm');
+const modalOverlay = document.getElementById('modalOverlay');
+const closeModalButton = document.getElementById('closeModal');
+
+// Função para abrir o modal
+function openModal() {
+    modalOverlay.classList.add('active');
+}
+
+// Função para fechar o modal
+function closeModal() {
+    modalOverlay.classList.remove('active');
+}
+
+// Adiciona evento para abrir o modal
+openModalButtons.forEach(button => {
+    button.addEventListener('click', openModal);
+});
+
+// Fecha o modal ao clicar no botão de fechar
+closeModalButton.addEventListener('click', closeModal);
+
+// Fecha o modal ao clicar na overlay
+modalOverlay.addEventListener('click', (event) => {
+    if (event.target === modalOverlay) {
+        closeModal();
+    }
+});
